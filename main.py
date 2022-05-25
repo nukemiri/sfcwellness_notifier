4ef9bedc823f9182adf95dc967d53bc8a365b11c
nukemiri <60301509+nukemiri@users.noreply.github.com>
2022-05-26 06:24:06 +0900

    twitter機能削除

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from re import A
 import requests
 import os
 from bs4 import BeautifulSoup
@@ -8,45 +9,46 @@ from pprint import pprint
 import json
 from pathlib import Path
 import datetime
+# import tweepy
+import config
 
 
 
-url = "https://wellness.sfc.keio.ac.jp/v3/index.php"
-empty_url = "https://wellness.sfc.keio.ac.jp/v3/index.php?page=reserve&limit=9999"
-session = requests.session()
-login_data = {
-    'UTF-8': '✓',
-    'login': os.environ.get("CNS_LOGIN_ID"),
-    'password': os.environ.get("CNS_LOGIN_PASSWORD"),
-    'submit':"login",
-    "page":"top",
-    "mode":"login",
-    }
-login = session.post(url, data=login_data)
-response = session.get(empty_url)
+url = "https://wellness.sfc.keio.ac.jp/v3/index.php?page=reserve&limit=9999"
+
+response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 table = soup.find_all("table")[1].tbody
 
+
 rows = table.find_all('tr')
 empty_list = []
 for row in rows:
-    column = []   
-    for td in row.find_all("td"):
+    column = []
+    tr = row.find_all("td")
+    for td in tr:
         column.append(td.text.replace('\r', '').replace('\n', '').replace('\u3000', ''))
     empty_list.append(column)
+
+"""<td class="w3-hide-small center"></td>
+<td class="w3-hide-small center"></td>
+<td class="w3-hide-small">体操</td>
+<td class="w3-hide-small">久永　将太</td>
+<td class="w3-hide-small center"><small>SFCアリーナ</small></td>
+<td class="w3-hide-small center">中</td>
+<td class="w3-hide-large w3-hide-medium"><strong>日時:</strong> 6月7日(火) 3限<br/><strong>種目:</strong> 体操<br/><strong>教員:</strong> 久永　将太<br/><strong>場所:</strong> SFCアリーナ<br/><strong>運動強度:</strong> 中</td>
+<td class="center">10</td>"""
+
+pprint(empty_list)
 
 for i in range(len(empty_list)):
     if empty_list[i][0] == "":
         empty_list[i][0] = empty_list[i-1][0]
         if empty_list[i][1] == "":
             empty_list[i][1] = empty_list[i-1][1]
-    if empty_list[i][9] == "向き":
-        empty_list[i][9] = "運動制限向き"
-    elif empty_list[i][9] == "限定":
-        empty_list[i][9] = "運動制限限定"
-# [0日時,1時限,2種目名,3教員名,4シラバスリンク,5実施形態,6場所,7開講言語,8運動強度,9運動制限,10空き,11予約リンク]
-# pprint(empty_list)
+# [0日付,1時限,2種目名,3教員名,4場所,5運動強度,6'日時: 6月8日(水) 5限種目: フェンシング教員: 冨田智子場所: SFC剣道場運動強度: 中',7空き]
+pprint(empty_list)
 
 myfile = Path(os.path.join(os.path.dirname(__file__),"lastempty.txt"))
 myfile.touch(exist_ok=True)
@@ -72,14 +74,29 @@ pprint(notify_list)
 
 if len(notify_list)>0:
     # 通知する
-    if os.environ.get("WELLNESS_NOTIFIER_WEBHOOK_URL") != None:
-        dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
-        for i in notify_list:
-            webhook_url  = os.environ.get("WELLNESS_NOTIFIER_WEBHOOK_URL")
-            main_content = {'content': f"""
+    dt_now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9)))
+
+    # if config.ACCESS_TOKEN != "":
+    #         CK = config.CONSUMER_KEY
+    #         CS = config.CONSUMER_SECRET
+    #         AT = config.ACCESS_TOKEN
+    #         AS = config.ACCESS_TOKEN_SECRET
+    #         auth = tweepy.OAuthHandler(CK, CS)
+    #         auth.set_access_token(AT, AS)
+    #         api = tweepy.API(auth)
+
+    for i in notify_list:
+        message = f"""
 {i[0]}{i[1]}『{i[2]}』が予約可能になりました。
-場所:{i[6]} 強度:{i[8]} {i[9]} [{dt_now.strftime('%H:%M')}]
+場所:{i[4]} 強度:{i[5]} [{dt_now.strftime('%H:%M')}]
 https://wellness.sfc.keio.ac.jp/
-            """}
+    """
+        if config.DISCORD_WEBHOOK != "":
+            
+            webhook_url  = config.DISCORD_WEBHOOK
+            main_content = {'content': message}
             headers      = {'Content-Type': 'application/json'}
             response     = requests.post(webhook_url, json.dumps(main_content), headers=headers)
+        # if config.ACCESS_TOKEN != "":
+        #     api.update_status(message)
+    
